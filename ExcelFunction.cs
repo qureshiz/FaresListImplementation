b3e4c294464c89fa3ff370fbafee92845a360529
Zishan Qureshi <zishan.qureshi@gmail.com>
2019-03-28 20:27:57 +0000

    Create Excel Application

--- ExcelFunction.cs
+++ ExcelFunction.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using Excel = Microsoft.Office.Interop.Excel; // Require for Excel.Application instantiation.
 
 
 public class ExcelFunction
 {
 	public CreateExcelApplication()
-    {
+	{
+	    var excelApp = new Excel.Application();
+	    
+	    // Quit Excel Application.
+	    excelApp.Quit();
 
-	}
+	    // Clean Up.
+	    releaseObject(xlApp);
+	    releaseObject(xlWorkBook);
+    }
 }
