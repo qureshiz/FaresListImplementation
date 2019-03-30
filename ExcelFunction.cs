0c9e69582652f2f1c4151e30806f4eaeb11405c1
qureshiz <41581171+qureshiz@users.noreply.github.com>
2019-03-30 11:34:19 +0000

    Added code to open workbook and read a cell.
    C# Excel Tutorial - #1 - Open and Read Excel Files
    https://youtu.be/lsv7rAsvYuA

--- ExcelFunction.cs
+++ ExcelFunction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using Excel = Microsoft.Office.Interop.Excel; // Require for Excel.Application instantiation.
-
+using _Excel = Microsoft.Office.Interop.Excel; // Require for Excel.Application instantiation.  This creates an object Excel.
+//Should we also have a using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
 
 public class ExcelFunction
 {
@@ -15,5 +16,34 @@ public class ExcelFunction
 	    releaseObject(xlApp);
 	    releaseObject(xlWorkBook); 
         //test
-    }
+    	}
+	
+class Excel
+{
+	string path = ""; //Path to Excel file.
+	_Application excel = new _Excel.Application();
+	workbook wb;
+	worksheet ws;
+	
+	public Excel(string path, in sheet)
+	{
+		this.path = path;
+		wb = excel.Workbooks.Open(path);
+		ws = wb.Worksheets[sheet]; // Not sure if this is a square or curly bracket.
+	}
+	
+	public string ReadCell(int i, int j) // row and column reference.
+	{
+		//Note, excel reference is 1 based.
+		i++;
+		j++;
+		//ensure cell is not null.
+		if(ws.Cells[i,j].value2 != null) // value2 is the value inside the cell.
+			return ws.cells[i,j].value2;
+		else
+			return "";
+			
+	}
+}
+	
 }
