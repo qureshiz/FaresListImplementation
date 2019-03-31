4a8fd9da462154b2cee75fe383c316ac87656aa4
Zishan Qureshi <zishan.qureshi@gmail.com>
2019-03-31 17:40:45 +0100

    Added FaresListImplementation project. Added FileFunctions Class to FaresListImplementation project. Created Unit Test to test if File Exists.

---
+++ Fares List Implementation Test/FaresListImplementationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using FaresListImplementation;
+
+namespace FaresListImplementation.Tests
+{
+    [TestFixture]
+    public class FileTests
+    {
+        [Test]
+        public void TestFileExists()
+        {
+           var fileFunction = new FileFunctions();
+            string fileName = @"c:\temp\FaresListImplementation.xlsx";
+            Assert.IsTrue(fileFunction.CheckFileExists(fileName));
+        }
+    }
+}
---
+++ FaresListImplementation/FileFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+//using _Excel = Microsoft.Office.Interop.Excel; // Require for Excel.Application instantiation.  This creates an object Excel.
+//Should we also have a using Microsoft.Office.Interop.Excel;
+//using Microsoft.Office.Interop.Excel;
+
+namespace FaresListImplementation
+{
+    public class FileFunctions
+    {
+        public bool CheckFileExists(string path)
+        {
+            //Hard code the name of a file for now.
+
+            return File.Exists(path);
+        }
+    }
+
+
+}
